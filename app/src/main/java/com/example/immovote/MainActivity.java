76c6a655539bf5e2df5c22c3049c15433086a90b
ThiloPaetzel <thilopaetzel@gmail.com>
2023-05-15 09:25:35 +0200

    Project is setup

---
+++ app/src/main/java/com/example/immovote/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.immovote;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+
+    private FirebaseAuth mAuth = FirebaseAuth.getInstance();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
